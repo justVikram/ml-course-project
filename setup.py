a4fc5bb5b71a1304185e9febdc49bf1a02840099
Vikram <59924856+justVikram@users.noreply.github.com>
2021-12-08 17:24:18 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ML course project, 5th semester, 2021.',
+    author='D1',
+    license='MIT',
+)
