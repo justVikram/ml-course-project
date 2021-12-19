c90eda016e2a0c3e64b1150c14e9127bff1bc755
Vikram <59924856+justVikram@users.noreply.github.com>
2021-12-19 14:20:28 +0530

    add code to split data into train and test directories

--- src/preprocess/make_dataset.py
+++ src/preprocess/make_dataset.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+import numpy
 
 
 def traverse_dir(path: os.path, extension: str = 'jpg') -> None:
@@ -7,7 +9,7 @@ def traverse_dir(path: os.path, extension: str = 'jpg') -> None:
         if directory.find('.') == -1:
             assign_seq_file_names(os.path.join(path, directory))
             change_img_extension(os.path.join(path, directory), extension)
-            resize_img(os.path.join(path, directory), 512)
+            resize_img(os.path.join(path, directory))
 
 
 def change_img_extension(path: os.path, extension: str) -> None:
@@ -27,18 +29,31 @@ def assign_seq_file_names(path: os.path) -> None:
             os.rename(os.path.join(path, file_name), os.path.join(path, new_file_name))
 
 
-def resize_img(path: os.path, size: int) -> None:
+def split_into_test_train(data_path: os.path, test_path: os.path, test_size: float) -> None:
+    subcategories = ['mall', 'college', 'office']
+    for subcategory in subcategories:
+        source = os.path.join(data_path, subcategory)
+        dest = os.path.join(test_path, subcategory)
 
+        for file in os.listdir(source):
+            if file.find('.') != 0:
+                if numpy.random.rand(1) < test_size:
+                    shutil.move(os.path.join(source, file), os.path.join(dest, file))
+
+
+def resize_img(path: os.path, size: int = 32) -> None:
     # Resize images in the dataset to {size}
     for img_name in os.listdir(path):
         if img_name.find('.') != 0:
             # To force the size on both dimensions, use 'size x size!'
-            os.system(f"convert {os.path.join(path, img_name)} -resize '{size}x{size}' {os.path.join(path, img_name)}")
+            os.system(f"convert {os.path.join(path, img_name)} -resize '{size}x{size}!' {os.path.join(path, img_name)}")
+
 
 if __name__ == '__main__':
     train_path = './data/train'
     test_path = './data/test'
     extn = 'jpg'
 
+    split_into_test_train(train_path, test_path, 0.2)
     traverse_dir(train_path, extn)
     traverse_dir(test_path, extn)
