00267403d7a2ac55351ccbb9c58628d5a75d7040
Vikram <59924856+justVikram@users.noreply.github.com>
2021-12-19 16:17:51 +0530

    refactor code

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,15 +1,17 @@
-import copy
-import time
 import torch
 import torch.nn as nn
-from torchvision import datasets, models, transforms
-import numpy as np
-from torch import optim
+import torch.optim as optim
 from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
 import os
+import copy
 
-mean = np.array([0.485, 0.456, 0.406])
-std = np.array([0.229, 0.224, 0.225])
+mean = np.array([0.5, 0.5, 0.5])
+std = np.array([0.25, 0.25, 0.25])
 
 data_transforms = {
     'train': transforms.Compose([
@@ -26,23 +28,37 @@ data_transforms = {
     ]),
 }
 
-data_dir = r'C:\Users\RISHABJAIN\OneDrive\Documents\dataset'
+data_dir = r'G:\ML_01\data'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'val']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                              shuffle=True, num_workers=0)
+               for x in ['train', 'val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names = image_datasets['train'].classes
 
-sets = ['train', 'val']
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(class_names)
 
-image_datasets = {
-    x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
-    for x in ['train', 'val']
-}
 
-dataloaders = {
-    x: torch.utils.data.DataLoader(
-        image_datasets[x], batch_size=4, shuffle=True, num_workers=0)
-    for x in ['train', 'val']
-}
+def imshow(inp, title):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    plt.title(title)
+    plt.show()
 
-datasets_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-class_name = image_datasets['train'].classes
+
+# Get a batch of training data
+inputs, classes = next(iter(dataloaders['train']))
+
+# Make a grid from batch
+out = torchvision.utils.make_grid(inputs)
+
+imshow(out, title=[class_names[x] for x in classes])
 
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
@@ -52,63 +68,93 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     best_acc = 0.0
 
     for epoch in range(num_epochs):
-        print(f'Epoch{epoch}/{num_epochs - 1}')
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
 
+        # Each epoch has a training and validation phase
         for phase in ['train', 'val']:
             if phase == 'train':
-                model.train()
+                model.train()  # Set model to training mode
             else:
-                model.eval()
+                model.eval()  # Set model to evaluate mode
 
             running_loss = 0.0
             running_corrects = 0
 
+            # Iterate over data.
             for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # forward
+                # track history if only in train
                 with torch.set_grad_enabled(phase == 'train'):
                     outputs = model(inputs)
                     _, preds = torch.max(outputs, 1)
                     loss = criterion(outputs, labels)
 
+                    # backward + optimize only if in training phase
                     if phase == 'train':
                         optimizer.zero_grad()
                         loss.backward()
                         optimizer.step()
 
+                # statistics
                 running_loss += loss.item() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
 
             if phase == 'train':
                 scheduler.step()
 
-            epoch_loss = running_loss / datasets_sizes[phase]
-            epoch_acc = running_corrects.double() / datasets_sizes[phase]
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
 
-            print(f'{phase} Loss: {epoch_loss:.4f} Acc{epoch_acc:.4f}')
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
 
+            # deep copy the model
             if phase == 'val' and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best_model_wts = copy.deepcopy(model.state_dict())
 
         print()
 
-        time_elapsed = time.time() - since
-        print(f'training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
-        print(f'best val acc : {best_acc:4f}')
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
 
-        model.load_state_dict(best_model_wts)
-        return model
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
 
 
+def predict(model, image_path):
+    import PIL.Image as Image
+    model.eval()
+    image = Image.open(image_path)
+    image = data_transforms['val'](image).float()
+    image = image.unsqueeze(0)
+    image = image.to(device)
+    output = model(image)
+    _, preds = torch.max(output, 1)
+
+    print(class_names[preds.item()])
+
+
+# Load a pretrained model and reset final fully connected layer.
 model = models.squeezenet1_0(pretrained=True)
 
-for param in model.parameters():
-    param.requires_grad = False
-
 num_ftrs = model.classifier[1].in_channels
+
 model.classifier[1] = nn.Conv2d(num_ftrs, 3, kernel_size=(1, 1), stride=(1, 1))
+
+model = model.to(device)
+
 criterion = nn.CrossEntropyLoss()
+
 optimizer = optim.SGD(model.parameters(), lr=0.001)
-step_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7,
-                                        gamma=0.1)
-model = train_model(model, criterion, optimizer, step_lr_scheduler, num_epochs=20)
+
+step_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+model = train_model(model, criterion, optimizer, step_lr_scheduler, num_epochs=25)
